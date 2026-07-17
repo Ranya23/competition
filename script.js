6d22ad18adc8d6010010544e7b80d56c872981a2
Ranya23 <kirdstan.hassan@gmail.com>
2026-07-17 17:19:18 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+// Store credentials globally so the Vue app can access them
+window.AdminCredentials = {
+    email: 'admin@learningjourney.iq',
+    password: 'SuperSecretPassword123'
+};
